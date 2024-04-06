5696305babcd5b3c065e21426c86d09913a03e6b
DarkriderSH7 <suhaib.hussain26000@gmail.com>
2024-04-06 14:33:31 -0400

    Commit with 10 test cases running

--- InsuranceQoute.cs
+++ InsuranceQoute.cs
@@ -23,6 +23,13 @@ namespace Assignment4
         }
 
         [Test]
+        public void InsuranceQuote01_ValidDataAge24Exp3Accidents0()
+        {
+            FillForm("SyedSuhaib", "Hussain", "24", "3", "0");
+            AssertValidSubmission("$4000"); 
+        }
+
+        [Test]
         public void InsuranceQuote02_ValidDataAge25Exp3Accidents4()
         {
             FillForm("SyedSuhaib", "Hussain", "25", "3", "4");
